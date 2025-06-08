4a4c0c624ed65beda50231ac6be9edeb63934bcb
pavel <pavel_chermyanin@mail.ru>
2025-06-08 03:24:33 +0300

    added new sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import styles from './App.module.scss'
 import { Header } from "./components/header/Header.jsx";
 import {Top} from "./components/top/Top.jsx";
 import {Profile} from "./components/profile/Profile.jsx";
+import {Info} from "./components/info/Info.jsx";
+import {DressCode} from "./components/dress-code/DressCode.jsx";
+import omniWorld from './assets/omniverse-world.webp'
 
 function App() {
 
@@ -12,6 +15,10 @@ function App() {
       <div className={styles.bg_1}>
         <Top/>
         <Profile/>
+        <Info/>
+        {/*<div className={styles.omniWorld}/>*/}
+        <img className={styles.omniWorld} src={omniWorld} alt=""/>
+        <DressCode/>
       </div>
     </div>
   )
---
+++ src/components/dress-code/DressCode.jsx
@@ -0,0 +1,14 @@
+import styles from './dressCode.module.scss'
+import {Button} from "../button/Button.jsx";
+
+
+export const DressCode = () => {
+
+  return (
+    <div className={styles.dress_code}>
+
+
+      <h2 className={styles.dress_code_title}>ДРЕСС-КОД</h2>
+    </div>
+  );
+};
