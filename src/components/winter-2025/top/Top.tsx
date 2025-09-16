d192a98e14c0519713a7f86b6534eab9bcb781be
pavel <pavel_chermyanin@mail.ru>
2025-09-16 19:01:06 +0300

    added animation top

--- src/components/winter-2025/top/Top.tsx
+++ src/components/winter-2025/top/Top.tsx
@@ -1,14 +1,25 @@
+// Top.tsx
 import styles from './top.module.scss';
 import React from 'react'
 import face from '../../../assets/winter/face.png'
+import arm from '../../../assets/winter/arm.png'
 
 export const Top = () => {
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.title_wrapper}>
-                <img className={styles.img_face} src={face}/>
-                <h1 className={styles.title}>Digital Elka</h1>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title_wrapper}>
+        <img
+          className={styles.img_face}
+          src={face}
+          alt="Лицо снеговика"
+        />
+        <img
+          className={styles.img_arm}
+          src={arm}
+          alt="Рука снеговика"
+        />
+        <h1 className={styles.title}>Digital Elka</h1>
+      </div>
+    </div>
+  )
 }
